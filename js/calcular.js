c5de29cd38ca153bec6ade8fd4bd90506e5757fa
diegodonascimentoconfessor <153656081+diegodonascimentoconfessor@users.noreply.github.com>
2024-12-13 15:31:36 -0300

    realizar o teste

--- js/calcular.js
+++ js/calcular.js
@@ -2,16 +2,19 @@ let operacao = "";
 let valor1 = "";
 let valor2 = "";
 
+// eslint-disable-next-line no-unused-vars
 function adicionarNumero(numero) {
     document.getElementById("input").value += numero;
 }
 
+// eslint-disable-next-line no-unused-vars
 function definirOperacao(op) {
     valor1 = document.getElementById("input").value;
     operacao = op;
     document.getElementById("input").value = "";
 }
 
+// eslint-disable-next-line no-unused-vars
 function calcularResultado() {
     valor2 = document.getElementById("input").value;
     let resultado;
@@ -34,6 +37,7 @@ function calcularResultado() {
     document.getElementById("input").value = resultado;
 }
 
+// eslint-disable-next-line no-unused-vars
 function limparDisplay() {
     document.getElementById("input").value = "";
     valor1 = "";
